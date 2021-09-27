0a4d5c06b9bb9e96c8a175749c841854af26a8ae
JmA515 <janderson515@yahoo.com>
2021-09-27 14:25:03 -0700

    Paired programming with Garrett. Added edit_profile method to views.py in employees app.

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -1,6 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.apps import apps
+from .models import Employee
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
@@ -11,3 +15,20 @@ def index(request):
     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
     Customer = apps.get_model('customers.Customer')
     return render(request, 'employees/index.html')
+
+@login_required
+def edit_profile(request):
+    logged_in_user = request.user
+    logged_in_employee = Employee.objects.get(user=logged_in_user)
+    if request.method == "POST":
+        name_from_form = request.POST.get('name')
+        zip_from_form = request.POST.get('zip_code')
+        logged_in_employee.name = name_from_form
+        logged_in_employee.zip_code = zip_from_form
+        logged_in_employee.save()
+        return HttpResponseRedirect(reverse('employees:index'))
+    else:
+        context = {
+            'logged_in_employee': logged_in_employee
+        }
+        return render(request, 'employees/edit_profile.html', context)
